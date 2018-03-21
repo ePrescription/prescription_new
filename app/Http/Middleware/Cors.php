aa1229d67f024c2229dd02278c2389cf972f5ac2
RithiBaskaran <baskar2271@yahoo.com>
2018-03-21 15:07:58 +0530

    Modified comments

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,16 +15,16 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        return $next($request)
+        /*return $next($request)
             ->headers->set('Access-Control-Allow-Origin', '*')
             ->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-            ->headers->set('Access-Control-Allow-Headers', '*');
+            ->headers->set('Access-Control-Allow-Headers', '*');*/
 
-        /*$response = $next($request);
+        $response = $next($request);
         $response->headers->set('Access-Control-Allow-Origin', '*');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', '*');
 
-        return $response;*/
+        return $response;
     }
 }
