be626133fa177bf736bacf848cd3b62fcad0feac
RithiBaskaran <baskar2271@yahoo.com>
2018-07-07 18:04:50 +0530

    Fixed the lab reports upload issue by doctor. Doctor should be able to upload multiple reports.

---
+++ app/prescription/utilities/DoctorFeepaymentStatus.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 06/07/2018
+ * Time: 3:11 PM
+ */
+
+namespace App\prescription\utilities;
+
+
+class DoctorFeepaymentStatus {
+
+    const DOCTOR_FEE_UNPAID_STATUS = 0;
+    const DOCTOR_FEE_PAID_STATUS = 1;
+}
